26e8555868b30a6671309d65680a39c6280c378e
Michael <skoda.michael@gmail.com>
2018-11-09 19:53:37 +0100

    complete Edge

--- src/drawables/Edge.java
+++ src/drawables/Edge.java
@@ -17,29 +17,68 @@ public class Edge {
     }
 
     public void order(){
-        // TODO seřadit dle y1 < y2
+        //seřadit dle y1 < y2
+        if (y1 > y2){
+            int pom = y2;
+            y2 = y1;
+            y1 = pom;
+            pom = x2;
+            x2 = x1;
+            x1 = pom;
+        }
+
     }
 
     public void cut(){
-        // todo oříznout poslední pixel
+        // oříznout poslední pixel
+        y2 =-1;
+        x2 =-1;
     }
 
     public void compute(){
-        // todo vypočítat k a q
+        // vypočítat k a q
+        int dy = y2 - y1;
+        int dx = x2 - x1;
+        k = dy / (float) dx;
+        q = dy - k*dx;
+
     }
 
     public int findX(int y){
-        return 0; // TODO vypočítat X dle y,k,q
+        // vypočítat X dle y,k,q
+        // y = kx + q
+        int x = (int) ((y - q)/k);
+        return x;
     }
 
     public boolean isIntersection(int y){
-        return false; //TODO - true když y > y1 && y < y2
+        // - true když y > y1 && y < y2
+        if (y > y1 && y < y2){
+            return true;
+        } else{
+            return false;
+        }
+
     }
 
     public int yMin(int yMin){
-        return 0; //todo dle y1, y2 a yMin rozhodnout které vracíme
+        //dle y1, y2 a yMin rozhodnout které vracíme
+        if (y1 < yMin){
+            yMin = y1;
+        }
+        if (y2 < yMin){
+            yMin = y2;
+        }
+        return yMin;
     }
     public int yMax(int yMax){
-        return 0; //todo dle y1,y2,yMax navrátit maxHodnotu
+        //dle y1,y2,yMax navrátit maxHodnotu
+        if (y1 > yMax){
+            yMax = y1;
+        }
+        if (y2 > yMax){
+            yMax = y2;
+        }
+        return yMax;
     }
 }
